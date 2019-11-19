450388efaca5c1200e33ca8f6b5816f34ebe3eb6
Cp <raulcape.7@gmail.com>
2019-11-19 22:41:34 +0100

    SQL document and empty project

---
+++ src/main/java/ule/inso1/Farmacia/App.java
@@ -0,0 +1,13 @@
+package ule.inso1.Farmacia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
